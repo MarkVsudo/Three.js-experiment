44e4c4ceb4fe62e6dc87e524fd36b96de8b1e25e
Mark Veskov <mark.veskov@abv.bg>
2023-07-01 13:18:17 +0300

    Some changes

--- credits.md
+++ credits.md
@@ -17,3 +17,6 @@
 "Pile of construction ruined debris bricks" (https://skfb.ly/oDwpO) by axonite is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
 
 "Construction debris" (https://skfb.ly/oAUzU) by conflig is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+
+
+"Antique wooden desk with props" (https://skfb.ly/oHrDH) by shuvalov.di is licensed under CC Attribution-NonCommercial-ShareAlike (http://creativecommons.org/licenses/by-nc-sa/4.0/).
--- public/app.js
+++ public/app.js
@@ -1,97 +1,44 @@
-// // Import the THREE.js library
-// import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
-// import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+// Import Three.js and any required modules
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-// const scene = new THREE.Scene();
-// const camera = new THREE.PerspectiveCamera(
-//   75,
-//   window.innerWidth / window.innerHeight,
-//   0.1,
-//   1000
-// );
+// Get the container element
+const container = document.getElementById("canvas-container");
 
-// // Move each object to different corners
-// const objectPosition1 = new THREE.Vector3(-30, -10, 10);
-// const objectPosition2 = new THREE.Vector3(20, -20, 0);
-// const objectPosition3 = new THREE.Vector3(20, 20, 0);
-// const objectPosition4 = new THREE.Vector3(-20, 20, 0);
+// Create a WebGL renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+container.appendChild(renderer.domElement);
 
-// let mouseX = window.innerWidth / 2;
-// let mouseY = window.innerHeight / 2;
+// Create a new scene
+const scene = new THREE.Scene();
 
-// let objToRender = "model1";
+// Create a camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.set(0, 0, 5);
 
-// const loader = new GLTFLoader();
-// loadModel("model1", objectPosition1);
-// loadModel("model3", objectPosition2);
-// loadModel("model4", objectPosition3);
-// loadModel("model5", objectPosition4);
+// Load the 3D model
+const loader = new GLTFLoader();
+loader.load("path/to/your/3d-model.gltf", (gltf) => {
+  scene.add(gltf.scene);
+});
 
-// function loadModel(modelName, position) {
-//   loader.load(
-//     `public/assets/${modelName}/scene.gltf`,
-//     function (gltf) {
-//       const object = gltf.scene;
-//       object.position.copy(position);
-//       scene.add(object);
-//     },
-//     function (xhr) {
-//       console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-//     },
-//     function (error) {
-//       console.error(error);
-//     }
-//   );
-// }
+// Add lights to the scene (optional but recommended)
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
 
-// const renderer = new THREE.WebGLRenderer({ alpha: true });
-// renderer.setSize(window.innerWidth, window.innerHeight);
-// document.getElementById("model-container").appendChild(renderer.domElement);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(0, 2, 2);
+scene.add(directionalLight);
 
-// // Set camera position and target to encompass all models
-// const cameraTarget = new THREE.Vector3(0, 0, 0); // Center of the scene
-// const distanceBetweenModels = 20; // Adjust this distance as needed
-
-// // Calculate bounding sphere containing all models
-// const boundingSphere = new THREE.Sphere(cameraTarget, distanceBetweenModels);
-
-// // Update camera position and target to include all models in view
-// camera.position.set(0, 0, boundingSphere.radius * 2);
-// camera.lookAt(cameraTarget);
-
-// const topLight = new THREE.DirectionalLight(0xffffff, 1);
-// topLight.position.set(500, 500, 500);
-// topLight.castShadow = true;
-// scene.add(topLight);
-
-// const ambientLight = new THREE.AmbientLight(
-//   0x333333,
-//   objToRender === "model2" ? 5 : 1
-// );
-// scene.add(ambientLight);
-
-// // Disable OrbitControls
-// // const controls = new OrbitControls(camera, renderer.domElement);
-// // Remove the above line to disable the OrbitControls
-
-// function animate() {
-//   requestAnimationFrame(animate);
-
-//   // Remove the controls.update() function to disable zooming and dragging
-//   // controls.update();
-
-//   renderer.render(scene, camera);
-// }
-
-// window.addEventListener("resize", function () {
-//   camera.aspect = window.innerWidth / window.innerHeight;
-//   camera.updateProjectionMatrix();
-//   renderer.setSize(window.innerWidth, window.innerHeight);
-// });
-
-// document.onmousemove = (e) => {
-//   mouseX = e.clientX;
-//   mouseY = e.clientY;
-// };
-
-// animate();
+// Render the scene
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
