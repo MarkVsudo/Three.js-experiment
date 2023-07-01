5e9aa217e4d93796cc6975d83a65beaaddb16e9f
Mark Veskov <mark.veskov@abv.bg>
2023-07-01 13:59:53 +0300

    Imported 3d model

--- public/app.js
+++ public/app.js
@@ -1,44 +1,116 @@
-// Import Three.js and any required modules
-import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+// Import the THREE.js library
+import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
 
-// Get the container element
-const container = document.getElementById("canvas-container");
+// To allow for the camera to move around the scene
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
 
-// Create a WebGL renderer
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-container.appendChild(renderer.domElement);
+// To allow for importing the .gltf file
+import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
-// Create a new scene
+// Create a Three.JS Scene
 const scene = new THREE.Scene();
 
-// Create a camera
+// Create a new camera with positions and angles
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-camera.position.set(0, 0, 5);
 
-// Load the 3D model
+// Keep track of the mouse position, so we can make the eye move
+let mouseX = window.innerWidth / 2;
+let mouseY = window.innerHeight / 2;
+
+// Keep the 3D object on a global variable so we can access it later
+let object;
+
+// OrbitControls allow the camera to move around the scene
+let controls;
+
+// Set which object to render
+let objToRender = "desk";
+
+// Instantiate a loader for the .gltf file
 const loader = new GLTFLoader();
-loader.load("path/to/your/3d-model.gltf", (gltf) => {
-  scene.add(gltf.scene);
-});
 
-// Add lights to the scene (optional but recommended)
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+// Load the file
+loader.load(
+  `/public/assets/${objToRender}/scene.gltf`,
+  function (gltf) {
+    // If the file is loaded, add it to the scene
+    object = gltf.scene;
+    scene.add(object);
+
+    // Move the 3D model lower by adjusting its position in the Y-axis
+    object.position.y = -0.7; // Adjust this value to move the model up or down
+  },
+  function (xhr) {
+    // While it is loading, log the progress
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  function (error) {
+    // If there is an error, log it
+    console.error(error);
+  }
+);
+
+// Instantiate a new renderer and set its size
+const renderer = new THREE.WebGLRenderer({ alpha: true }); //Alpha: true allows for the transparent background
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// Add the renderer to the DOM
+document.getElementById("container3D").appendChild(renderer.domElement);
+
+// Set how far the camera will be from the 3D model
+camera.position.z = objToRender === "desk" ? 2 : 10;
+
+// Set how far the camera will be from the 3D model
+camera.position.set(0, 0.6, 2); // Adjust the X, Y, and Z values to change the initial camera position
+camera.lookAt(0, 0, 0); // Set the point the camera looks at, in this case, the origin (0,0,0)
+
+// Add lights to the scene, so we can actually see the 3D model
+const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
+topLight.position.set(500, 500, 500); // top-left-ish
+topLight.castShadow = true;
+scene.add(topLight);
+
+const ambientLight = new THREE.AmbientLight(
+  0x333333,
+  objToRender === "desk" ? 5 : 1
+);
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-directionalLight.position.set(0, 2, 2);
-scene.add(directionalLight);
+// This adds controls to the camera, so we can rotate / zoom it with the mouse
+if (objToRender === "desk") {
+  controls = new OrbitControls(camera, renderer.domElement);
+}
 
 // Render the scene
 function animate() {
   requestAnimationFrame(animate);
+  // Here we could add some code to update the scene, adding some automatic movement
+
+  // Make the eye move (not applicable in this case since it's static now)
+  // if (object && objToRender === "desk") {
+  //   object.rotation.y = -3 + (mouseX / window.innerWidth) * 3;
+  //   object.rotation.x = -1.2 + (mouseY * 2.5) / window.innerHeight;
+  // }
   renderer.render(scene, camera);
 }
+
+// Add a listener to the window, so we can resize the window and the camera
+window.addEventListener("resize", function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+// Add mouse position listener, so we can make the eye move (not applicable in this case)
+// document.onmousemove = (e) => {
+//   mouseX = e.clientX;
+//   mouseY = e.clientY;
+// };
+
+// Start the 3D rendering
 animate();
